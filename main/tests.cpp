9df3a9a7e0a0af97ed6e06999e0a5dd74000dce9
Amit Mizrahi <amit@amizrahi.com>
2018-04-16 07:22:39 -0400

    Create test fixture, convert existing tests to gtest

--- main/tests.cpp
+++ main/tests.cpp
@@ -10,12 +10,27 @@
 #include "../include/PopulationGen.h"
 #include "../include/Terrain.h"
 
-using namespace MARS;
+#include "gtest/gtest.h"
 
-void matrixTest() {
+namespace {
+
+class MarsTest : public ::testing::Test {
+  protected:
+  MarsTest() {
+    // setup for each test
+  }
+
+  virtual ~MarsTest() {
+    // teardown for each test 
+  }
+    
+  // can declare objects here used by all tests
+};
+
+TEST_F(MarsTest, MatrixStoresData) {
   int num_rows = 32;
   int num_cols = 32;
-  Matrix<int> matrix(num_rows, num_cols);
+  MARS::Matrix<int> matrix(num_rows, num_cols);
   
   for (int i = 0; i < num_rows; i++) {
     for (int j = 0; j < num_cols; j++) {
@@ -25,25 +40,20 @@ void matrixTest() {
 
   for (int i = 0; i < num_rows; i++) {
     for (int j = 0; j < num_cols; j++) {
-      if (matrix.at(i, j) != i * j) {
-        std::cout << "Matrix test failed at index " << i << " " << j << std::endl;
-        std::exit(1);
-      }
+      EXPECT_EQ(matrix.at(i, j), i * j);
     }
   }
-
-  std::cout << "Matrix test successful" << std::endl;
 }
 
-void updatePopTest() {
+TEST_F(MarsTest, PopulationGenUpdatesPop) {
   int rows = 2;
   int cols = 2;
 
-  Matrix<double> matrix(rows, cols);
+  MARS::Matrix<double> matrix(rows, cols);
 
   for (int k=0; k<10; k++) {
     std::cout <<"round " << k << std::endl;
-    Matrix<double> newPop = PopulationGen().genNewPop(matrix);
+    MARS::Matrix<double> newPop = MARS::PopulationGen().genNewPop(matrix);
     for (int i=0; i<rows; i++) {
       for (int j=0; j<cols; j++ ) {
         std::cout << "new pop value at  (" << i << "," << j << "):" << newPop.at(i,j) << std::endl;
@@ -56,32 +66,24 @@ void updatePopTest() {
 }
 
 
-void bitMatrixTest() {
+TEST_F(MarsTest, BitMatrixStoresData) {
   int rows = 32;
   int cols = 32;
-  BitMatrix bitMatrix(rows, cols);
+  MARS::BitMatrix bitMatrix(rows, cols);
 
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
-      if (bitMatrix.get(i, j)) {
-        std::cout << "BitMatrix test default to zero failed at index " << i << " " << j << std::endl;
-      }
+      EXPECT_EQ(bitMatrix.get(i, j), 0);
       bitMatrix.set(i, j, true);
-      if (!bitMatrix.get(i, j)) {
-        std::cout << "BitMatrix test set to true failed at index " << i << " " << j << std::endl;
-      }
+      EXPECT_TRUE(bitMatrix.get(i, j));
       bitMatrix.set(i, j, false);
-      if (bitMatrix.get(i, j)) {
-        std::cout << "BitMatrix test set back to false failed at index " << i << " " << j << std::endl;
-      }
+      EXPECT_FALSE(bitMatrix.get(i, j));
     }
   }
-
-  std::cout << "BitMatrix test successful" << std::endl;
 }
 
-void terrainTest() {
-    Terrain terrain(8, 8);
+TEST_F(MarsTest, TerrainGen) {
+    MARS::Terrain terrain(8, 8);
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             std::cout << terrain.weightAtXY(i,j) << " ";
@@ -90,20 +92,20 @@ void terrainTest() {
     }
 }
 
-void coordTest() {
-  Coord c;
-  Coord c2(2, 2);
+TEST_F(MarsTest, Coords) {
+  MARS::Coord c;
+  MARS::Coord c2(2, 2);
 
-  std::vector<Coord> vectorCoord;
+  std::vector<MARS::Coord> vectorCoord;
   vectorCoord.push_back(c2);
 
-  std::unordered_set<Coord> setCoord;
+  std::unordered_set<MARS::Coord> setCoord;
   setCoord.insert(c2);
 }
 
-int main() {
-  matrixTest();
-  bitMatrixTest();
-  updatePopTest();
-    terrainTest();
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
