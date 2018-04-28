5e923717cae354052ff93c321b676149bb0b3b5b
Amit Mizrahi <amit@amizrahi.com>
2018-04-27 20:47:51 -0400

    Style fixes + remove commented-out code

--- include/Plant.h
+++ include/Plant.h
@@ -16,14 +16,21 @@ namespace MARS {
      */
     class Plant {
     public:
-        Coord location; //coordinates of plant in map
-        int in_service; //number of people currently serviced by plant
-        int capacity; //total number of people plant can service
-        double serviceable_distance; //furthest distance at which the plant can service
-        //std::vector<Coord> serviceableArea; //list of coordinates that plant can service
-        std::unordered_map<Coord, double> serviceable_area;
-        std::unordered_map<Coord, int> serviced_map; //contains mapping from coordinate to number of people being
-                                                    // serviced at that coordinate
+        /*
+         * location: coordinates of plant in map
+         * in_service: number of people currently serviced by plant
+         * capacity: total number of people plant can service
+         * serviceable_distance: furthest distance which the plant can service
+         * serviceable_area: coordinates that a plant can service
+         * serviced_map: mapping from a coordinate to number of people being serviced there
+         */
+        Coord location;
+        int in_service; 
+        int capacity;
+        double serviceable_distance; 
+        std::unordered_map<Coord, double> serviceable_area; 
+        std::unordered_map<Coord, int> serviced_map; 
+
         /*
          * Constructor
          */
@@ -34,18 +41,32 @@ namespace MARS {
                 int y,
                 Terrain &terrain);
 
+        /*
+         * Generate the plant's serviceable area given a terrain, location, and serviceable distance.
+         */
         std::unordered_map<Coord,double> generateServiceableArea(Terrain &terrain, Coord plant_loc, double serve_dist);
 
+        /*
+         * Initialize the serviced map using the plant's serviceable area.
+         */
         std::unordered_map<Coord, int> initializeServicedMap( std::unordered_map<Coord, double> serviceable_area);
 
 
+        /*
+         * Adds `pop' to THIS plant's serviced population.
+         */
         void addToService(int pop);
 
-        //Adds integer to THIS plant's serviced population
-        //Removes integer from inputted plant's serviced population
-        //if int is negative, this is equivalent to removing population from THIS plant's serviced population
+        /* Adds `pop' to THIS plant's serviced population and removes `pop' from the
+         * other plant's serviced population.
+         * If `pop' is negative, this is equivalent to removing population from THIS plant's 
+         * serviced population and adding it to the other.
+         */
         void changeService(Plant, int pop);
 
+        /*
+         * Adds a population at a given location to the plant's serviced map.
+         */
         void updateMapping(Plant plant, Coord person_loc, int pop);
 
         bool operator==(const Plant& other) const {
@@ -56,7 +77,7 @@ namespace MARS {
 
 namespace std {
     /*
-     * Hash a Coord instance
+     * Hash a Plant instance
      */
     template <>
     struct hash<MARS::Plant> {
--- src/Plant.cpp
+++ src/Plant.cpp
@@ -46,8 +46,8 @@ void Plant::addToService(int pop) {
 void Plant::updateMapping(Plant plant, Coord person_loc, int pop) {
   std::unordered_map<Coord,int>::const_iterator iter = plant.serviced_map.find(person_loc);
   if (iter == plant.serviced_map.end()) {
-    //add the entrance to the serviced map
-   plant.serviced_map.insert(std::pair<Coord,int>(person_loc, pop));
+    // add the entrance to the serviced map
+    plant.serviced_map.insert(std::pair<Coord,int>(person_loc, pop));
   }
   else {
     plant.serviced_map[person_loc] += pop;
@@ -62,42 +62,30 @@ std::unordered_map<Coord,double> Plant::generateServiceableArea(Terrain &terrain
     }
   }
 
- /* std::cout << "Terrain weight matrix: " << std::endl;
-
-  for (int i=0; i<terrain.sizeY; i++) {
-    for (int j=0; j<terrain.sizeX; j++) {
-      std::cout << "Coordinate (" << j << "," << i << "):" << terrain.weightAtXY(j, i) << std::endl;
-    }
-    std::cout << std::endl;
-  }*/
-
   std::queue<std::tuple<Coord, double>> queue;
   std::unordered_map<Coord, double> serviceable;
   queue.push(std::make_tuple(plantLoc, 0.0));
   visited.set(plantLoc.x, plantLoc.y, 1);
+
   while (queue.size() > 0) {
     std::tuple<Coord, double> locInfo = queue.front();
     double weightedDist = std::get<1>(locInfo);
     Coord loc = std::get<0>(locInfo);
-    //std::cout << "Coordinate in consideration: (" << loc.x << "," << loc.y << ")" << std::endl;
-    //std::cout << "weighted distance of this coordinate to plant = " << weightedDist << std::endl;
     queue.pop();
-    if (loc.y - 1 >= 0) { //within bounds
-      //std::cout << "Considering coordinate above, (" << loc.x << "," << loc.y-1 << ")" << std::endl;
-      if (visited.get(loc.x, loc.y-1) ==0) { //has not been visited
+    
+    if (loc.y - 1 >= 0) { // within bounds
+      if (visited.get(loc.x, loc.y-1) ==0) { // has not been visited
         double terrainWeight = terrain.weightAtXY(loc.x, loc.y-1);
-        if (weightedDist+terrainWeight <= serve_dist ) { //within service
+        if (weightedDist+terrainWeight <= serve_dist ) { // within service
           std::tuple<Coord, double> newLoc = std::make_tuple(Coord(loc.x, loc.y-1), weightedDist+terrainWeight);
           serviceable[Coord(loc.x, loc.y-1)] =  weightedDist+terrainWeight;
           queue.push(newLoc);
           visited.set(loc.x, loc.y-1,1);
-          //std::cout << "Added to serviceable area." << std::endl;
-
         }
       }
     }
+
     if (loc.y+1 < terrain.sizeY) {
-      //std::cout << "Considering coordinate below, (" << loc.x << "," << loc.y+1 << ")" << std::endl;
       if (visited.get(loc.x, loc.y+1) ==0) {
         double terrainWeight = terrain.weightAtXY(loc.x, loc.y+1);
         if (weightedDist + terrainWeight <= serve_dist) {
@@ -105,12 +93,11 @@ std::unordered_map<Coord,double> Plant::generateServiceableArea(Terrain &terrain
           serviceable[Coord(loc.x, loc.y+1)] =  weightedDist+terrainWeight;
           queue.push(newLoc);
           visited.set(loc.x, loc.y+1, 1);
-          //std::cout << "Added to serviceable area." << std::endl;
         }
       }
     }
+
     if (loc.x-1 >= 0) {
-      //std::cout << "Considering coordinate left, (" << loc.x-1 << "," << loc.y << ")" << std::endl;
       if (visited.get(loc.x-1, loc.y) == 0) {
         double terrainWeight = terrain.weightAtXY(loc.x-1, loc.y);
         if (weightedDist + terrainWeight <= serve_dist) {
@@ -118,24 +105,22 @@ std::unordered_map<Coord,double> Plant::generateServiceableArea(Terrain &terrain
           serviceable[Coord(loc.x-1, loc.y)] =  weightedDist+terrainWeight;
           queue.push(newLoc);
           visited.set(loc.x-1, loc.y, 1);
-          //std::cout << "Added to serviceable area." << std::endl;
         }
       }
     }
+
     if (loc.x+1 < terrain.sizeX) {
-      //std::cout << "Considering coordinate right, (" << loc.x+1 << "," << loc.y << ")" << std::endl;
       if (visited.get(loc.x+1, loc.y) ==0) {
         double terrainWeight = terrain.weightAtXY(loc.x+1, loc.y);
-        //std::cout << "Terrain weight = " << terrainWeight << std::endl;
         if (weightedDist + terrainWeight <= serve_dist) {
           std::tuple<Coord, double> newLoc = std::make_tuple(Coord(loc.x+1, loc.y), weightedDist+terrainWeight);
           serviceable[Coord(loc.x+1, loc.y)] =  weightedDist+terrainWeight;
           queue.push(newLoc);
           visited.set(loc.x+1, loc.y, 1);
-          //std::cout << "Added to serviceable area." << std::endl;
         }
       }
     }
+
   }
 
 
