1a359964981d8371cad7ee1c34672f7bea276f3f
Marissa Colasacco <marissacolasacco@dhcp-uris-2812.redrover.cornell.edu>
2018-05-04 09:09:07 -0400

    fixed game initialization, added some tests

--- main/tests.cpp
+++ main/tests.cpp
@@ -18,48 +18,48 @@
 
 namespace {
 
-  class MarsTest : public ::testing::Test {
+    class MarsTest : public ::testing::Test {
     protected:
-    MarsTest() {
-      // setup for each test
-    }
+        MarsTest() {
+          // setup for each test
+        }
 
-    virtual ~MarsTest() {
-      // teardown for each test 
-    }
-  
-    // can declare objects here used by all tests
-    MARS::PopulationMatrix popMat = MARS::PopulationMatrix(8, 8);
-    MARS::Game game = MARS::Game(8, 8, 100, 100, 50, 200, 200, 200);
-  };
+        virtual ~MarsTest() {
+          // teardown for each test
+        }
 
-  TEST_F(MarsTest, MatrixStoresData) {
-    int num_rows = 32;
-    int num_cols = 32;
-    MARS::Matrix<int> matrix(num_rows, num_cols);
-    
-    for (int i = 0; i < num_rows; i++) {
-      for (int j = 0; j < num_cols; j++) {
-        matrix.at(i, j) = i * j;
+        // can declare objects here used by all tests
+        MARS::PopulationMatrix popMat = MARS::PopulationMatrix(8, 8);
+        MARS::Game game = MARS::Game(8, 8, 100, 100, 50, 200, 200, 200);
+    };
+
+    TEST_F(MarsTest, MatrixStoresData) {
+      int num_rows = 32;
+      int num_cols = 32;
+      MARS::Matrix<int> matrix(num_rows, num_cols);
+
+      for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+          matrix.at(i, j) = i * j;
+        }
       }
-    }
 
-    for (int i = 0; i < num_rows; i++) {
-      for (int j = 0; j < num_cols; j++) {
-        EXPECT_EQ(matrix.at(i, j), i * j);
+      for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+          EXPECT_EQ(matrix.at(i, j), i * j);
+        }
       }
-    }
 
-    MARS::Matrix<int> matrix2 = matrix;
-    for (int i = 0; i < num_rows; i++) {
-      for (int j = 0; j < num_cols; j++) {
-        EXPECT_EQ(matrix2.at(i, j), i * j);
+      MARS::Matrix<int> matrix2 = matrix;
+      for (int i = 0; i < num_rows; i++) {
+        for (int j = 0; j < num_cols; j++) {
+          EXPECT_EQ(matrix2.at(i, j), i * j);
+        }
       }
-    }
-  };
+    };
 
 
-  TEST_F(MarsTest, GenerateServiceableAreaTest1_FlatLand) {
+    TEST_F(MarsTest, GenerateServiceableAreaTest1_FlatLand) {
       MARS::Terrain terrain(4);
 
       int cap = 10;
@@ -124,7 +124,7 @@ namespace {
 
 
     TEST_F(MarsTest, GenServiceableAreaTest4_SurroundedByWater) {
-      MARS::Terrain terrain(4,4, true);
+      MARS::Terrain terrain(4, 4, true);
 
       int cap = 10;
       double serv_dist = 4.0;
@@ -135,147 +135,178 @@ namespace {
       std::unordered_map<MARS::Coord, double> serviceableArea = plant.serviceableArea();
 
       EXPECT_EQ(0, serviceableArea.size());
-    };
+    }
 
-/*  TEST_F(MarsTest, PopulationGenUpdatesPop) {
+    /*
+  TEST_F(MarsTest, PopulationGenUpdatesPop) {
     int rows = 2;
     int cols = 2;
 
     MARS::Matrix<int> matrix(rows, cols);
     MARS::PopulationGen popGenerator = MARS::PopulationGen();
+    MARS::Terrain terrain = MARS::Terrain(rows,cols);
 
     for (int k=0; k<10; k++) {
-      matrix = popGenerator.generate(matrix);
+      matrix = popGenerator.generate(matrix, terrain);
       for (int i=0; i<rows; i++) {
         for (int j=0; j<cols; j++ ) {
           EXPECT_FALSE(std::isnan(matrix.at(i,j)));
         }
       }
     }
-  }*/
+  }
+     */
 
 
-  TEST_F(MarsTest, BitMatrixStoresData) {
-    int rows = 32;
-    int cols = 32;
-    MARS::BitMatrix bitMatrix(rows, cols);
+    TEST_F(MarsTest, BitMatrixStoresData) {
+      int rows = 32;
+      int cols = 32;
+      MARS::BitMatrix bitMatrix(rows, cols);
 
-    for (int i = 0; i < rows; i++) {
-      for (int j = 0; j < cols; j++) {
-        EXPECT_EQ(bitMatrix.get(i, j), 0);
-        bitMatrix.set(i, j, true);
-        EXPECT_TRUE(bitMatrix.get(i, j));
-        bitMatrix.set(i, j, false);
-        EXPECT_FALSE(bitMatrix.get(i, j));
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+          EXPECT_EQ(bitMatrix.get(i, j), 0);
+          bitMatrix.set(i, j, true);
+          EXPECT_TRUE(bitMatrix.get(i, j));
+          bitMatrix.set(i, j, false);
+          EXPECT_FALSE(bitMatrix.get(i, j));
+        }
       }
     }
-  }
 
-  TEST_F(MarsTest, TerrainGen) {
+    TEST_F(MarsTest, TerrainGen) {
       MARS::Terrain terrain(8, 8);
       for (int i = 0; i < 8; i++) {
-          for (int j = 0; j < 8; j++) {
-              EXPECT_FALSE(std::isnan(terrain.weightAtXY(i,j)));
-          }
-      }
-  }
-
-  TEST_F(MarsTest, Coords) {
-    MARS::Coord c1(2, 2);
-    MARS::Coord c2(2, 2);
-
-    EXPECT_EQ(c1, c2);
-  }
-
-  TEST_F(MarsTest, PopulationMatrixServicedByPlant) {
-    // sum up all plants, check that it's equal to total num serviced
-    MARS::Terrain terrain(8, 8);
-    MARS::Plant p1(100, 5, 2, 3, terrain);
-    MARS::Plant p2(100, 3, 4, 4, terrain);
-    MARS::Coord c(2, 3);
-
-    popMat.assignUnservicedPop(&p1, c, 5);
-    popMat.assignUnservicedPop(&p2, c, 10);
-
-    EXPECT_EQ(
-        popMat.numberServicedAtCoord(c),
-        popMat.numberServicedAtCoordByPlant(c, &p1) + popMat.numberServicedAtCoordByPlant(c, &p2)
-    );
-  }
-
-  TEST_F(MarsTest, MovePopNonZero) {
-    // move x from one plant to another
-    // check that population for plant 'from' has decreased by x
-    // and that population for plant 'to' has increased by x
-    MARS::Terrain terrain(8, 8);
-    MARS::Plant p1(100, 5, 2, 3, terrain);
-    MARS::Plant p2(100, 3, 4, 4, terrain);
-    MARS::Coord c(2, 3);
-
-    popMat.assignUnservicedPop(&p1, c, 5);
-    popMat.assignUnservicedPop(&p2, c, 10);
-    
-    int oldPop1 = popMat.numberServicedAtCoordByPlant(c, &p1);
-    int oldPop2 = popMat.numberServicedAtCoordByPlant(c, &p2);
-    
-    popMat.moveServicedPopBetweenPlants(&p2, &p1, c, 5);
-
-    int newPop1 = popMat.numberServicedAtCoordByPlant(c, &p1);
-    int newPop2 = popMat.numberServicedAtCoordByPlant(c, &p2);
-
-    EXPECT_EQ(
-        newPop1, oldPop1 + 5
-    );
-    EXPECT_EQ(
-        newPop2, oldPop2 - 5
-    );
-  }
-
-  // Q: are NULLs the best way to pass in nonexistent coords?
-
-  TEST_F(MarsTest, StepNoPlant) {
-    // Stepping without a plant added
-    game.step(false, MARS::Coord(NULL, NULL));
-    EXPECT_EQ(game.get_number_plants_in_service(), 0);
-  }
-
-  TEST_F(MarsTest, StepWithPlant) {
-    // Stepping with a plant added
-    game.step(true, MARS::Coord(3, 4));
-    EXPECT_NE(game.get_number_plants_in_service(), 0);
-  }
-
-  /* Q: what is in the first element of the pair when there is no plant? (cannot be NULL as NULL is numeric) */
-  
-  // Commenting this test out for now
-
-  /* TEST_F(MarsTest, FindBestPlantNoPlants) {
-    // No best plant if there are no plants
-    MARS::Coord arbitrary_coord(3, 4);
-    std::pair<MARS::Plant, bool> expected_result = std::pair<MARS::Plant, bool>(NULL, false);
-    EXPECT_EQ(
-        game.findBestPlant(arbitrary_coord), expected_result 
-    );
-  } */
-
-  TEST_F(MarsTest, FindBestPlant) {
-    for(int i = 0; i < 100; i++) {
-        game.step(false, MARS::Coord(NULL, NULL));
-    }
-    game.step(true, MARS::Coord(5, 5));
-    // after 100 steps, check to see if there's a "best plant" for some coord
-    bool foundPlant = false;
-    for(int i = 0; i < 8; i++) {
-        for(int j = 0; j < 8; j++) {
-            foundPlant = foundPlant || game.findBestPlant(MARS::Coord(i, j)).second;
+        for (int j = 0; j < 8; j++) {
+          EXPECT_FALSE(std::isnan(terrain.weightAtXY(i, j)));
         }
+      }
     }
-    EXPECT_TRUE(foundPlant);
-  }
+
+    TEST_F(MarsTest, Coords) {
+      MARS::Coord c1(2, 2);
+      MARS::Coord c2(2, 2);
+
+      EXPECT_EQ(c1, c2);
+    }
+
+    TEST_F(MarsTest, PopulationMatrixServicedByPlant) {
+      // sum up all plants, check that it's equal to total num serviced
+      MARS::Terrain terrain(8, 8);
+      MARS::Plant p1(100, 5, 2, 3, terrain);
+      MARS::Plant p2(100, 3, 4, 4, terrain);
+      MARS::Coord c(2, 3);
+
+      popMat.assignUnservicedPop(&p1, c, 5);
+      popMat.assignUnservicedPop(&p2, c, 10);
+
+      EXPECT_EQ(
+              popMat.numberServicedAtCoord(c),
+              popMat.numberServicedAtCoordByPlant(c, &p1) + popMat.numberServicedAtCoordByPlant(c, &p2)
+      );
+    }
+
+    TEST_F(MarsTest, MovePopNonZero) {
+      // move x from one plant to another
+      // check that population for plant 'from' has decreased by x
+      // and that population for plant 'to' has increased by x
+      MARS::Terrain terrain(8, 8);
+      MARS::Plant p1(100, 5, 2, 3, terrain);
+      MARS::Plant p2(100, 3, 4, 4, terrain);
+      MARS::Coord c(2, 3);
+
+      popMat.assignUnservicedPop(&p1, c, 5);
+      popMat.assignUnservicedPop(&p2, c, 10);
+
+      int oldPop1 = popMat.numberServicedAtCoordByPlant(c, &p1);
+      int oldPop2 = popMat.numberServicedAtCoordByPlant(c, &p2);
+
+      popMat.moveServicedPopBetweenPlants(&p2, &p1, c, 5);
+
+      int newPop1 = popMat.numberServicedAtCoordByPlant(c, &p1);
+      int newPop2 = popMat.numberServicedAtCoordByPlant(c, &p2);
+
+      EXPECT_EQ(
+              newPop1, oldPop1 + 5
+      );
+      EXPECT_EQ(
+              newPop2, oldPop2 - 5
+      );
+    }
+
+
+    TEST_F(MarsTest, InitializationTest) {
+      MARS::Game game2 = MARS::Game(8, 8, 1, 100, 50, 200, 200, 200);
+      EXPECT_EQ(game2.get_number_plants_in_service(), 0);
+    }
+    // Q: are NULLs the best way to pass in nonexistent coords?
+
+
+    TEST_F(MarsTest, StepNoPlant) {
+      // Stepping without a plant added
+      MARS::Game game1 = MARS::Game(8, 8, 1, 100, 50, 200, 200, 200);
+      game1.step(false, MARS::Coord(1, 1));
+      EXPECT_EQ(game1.get_number_plants_in_service(), 0);
+    }
+
+
+    TEST_F(MarsTest, StepWithPlant) {
+      // Stepping with a plant added
+      game.step(true, MARS::Coord(3, 4));
+      EXPECT_NE(game.get_number_plants_in_service(), 0);
+    }
+
+
+    /* Q: what is in the first element of the pair when there is no plant? (cannot be NULL as NULL is numeric) */
+
+    // Commenting this test out for now
+
+     TEST_F(MarsTest, FindBestPlantNoPlants) {
+      // No best plant if there are no plants
+      MARS::Coord arbitrary_coord(3, 4);
+      //std::pair<MARS::Plant, bool> expected_result = std::pair<MARS::Plant, bool>(NULL, false);
+      EXPECT_EQ(
+          game.findBestPlant(arbitrary_coord).second, false
+      );
+    }
+
+
+
+    TEST_F(MarsTest, FindBestPlant) {
+      for (int i = 0; i < 100; i++) {
+        game.step(false, MARS::Coord(1, 1));
+      }
+
+      for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+          std::cout << game.get_population_at(i, j) << " ";
+        }
+        std::cout << std::endl;
+      }
+      game.step(true, MARS::Coord(5, 5));
+      // after 100 steps, check to see if there's a "best plant" for some coord
+      bool foundPlant = false;
+      for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+          foundPlant = foundPlant || game.findBestPlant(MARS::Coord(i, j)).second;
+        }
+      }
+      EXPECT_TRUE(foundPlant);
+    }
+
+    TEST_F(MarsTest, BuildPlantOnAnother) {
+      game.step(true, MARS::Coord(1,1));
+      game.step(true, MARS::Coord(1,1));
+      game.step(true, MARS::Coord(1,1));
+      game.step(true, MARS::Coord(1,1));
+      EXPECT_EQ(game.get_number_plants_in_service(),1);
+    }
+
+
 
   TEST_F(MarsTest, UnservicedPopulationProcessed) {
     // check to see that number in service goes up over time
-    for(int i = 0; i < 50; i++) {
+    for(int i = 0; i < 15; i++) {
         if(i % 5 == 0) {
             game.step(true, MARS::Coord(i/5, i/5));
         }
@@ -285,6 +316,8 @@ namespace {
     }
     EXPECT_NE(game.get_number_pop_serviced(), 0);
   }
+
+/*
 
   TEST_F(MarsTest, ObjectiveFunctionValid) {
     for(int i = 0; i < 50; i++) {
@@ -299,7 +332,9 @@ namespace {
     EXPECT_NE(game.calculateObjective(), 0);
   }
 
+*/
 }
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
--- src/Game.cpp
+++ src/Game.cpp
@@ -5,6 +5,7 @@
 
 #include <queue>
 #include <limits>
+#include <iostream>
 
 using namespace MARS;
 
@@ -21,14 +22,16 @@ Game::Game(
     size_x(dx),
     size_y(dy),
     time(0),
-    plant_default_capacity(default_capacity),
     number_turns(number_turns),
     number_pop_serviced(0),
     number_plants_in_service(0),
+    number_new_plants(0),
+    plant_default_capacity(default_capacity),
     plant_servable_distance(serveable_distance),
     plant_initial_cost(initial_cost),
     plant_operating_cost(operating_cost),
     plant_profit_margin(profit_margin),
+    plants_in_service(std::vector<Plant*>() ),
     pop_matrix(PopulationMatrix(dx, dy)),
     terrain(Terrain(dx, dy)),
     pop_gen(PopulationGen())
@@ -41,73 +44,102 @@ int Game::get_number_plants_in_service() {
     return this->number_plants_in_service;
 }
 
+
+bool Game::check_if_plant_present(Coord coord) {
+  for (int i =0; i < this->plants_in_service.size(); i++) {
+    Plant* plant = this->plants_in_service[i];
+    if (coord==(plant->location)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+int Game::get_population_at(int i, int j) {
+  return this->pop_matrix.computeCombinedPop().at(i,j);
+}
+
+
+int Game::get_number_new_plants() {
+    return this->number_new_plants;
+};
+
 int Game::get_number_pop_serviced() {
     return this->number_pop_serviced;
 }
 
 void Game::step(bool add_plant, Coord plant_coord) {
+
     Matrix<int> new_population = pop_gen.generate(this->pop_matrix.computeCombinedPop(), this->terrain);
     pop_matrix.addUnservicedPop(new_population);
-
     processUnservicedPopulation();
     
     //if new plant was added
     if (add_plant) {
+      if (!check_if_plant_present(plant_coord)) {
         Plant* new_plant = createPlant(plant_coord);
-        std::queue<Plant*> touched_plant = considerNewPlant(new_plant, false);
-        processTouchedPlants(touched_plant);
+        std::queue<Plant*> touched_plants = considerNewPlant(new_plant, false);
+        processTouchedPlants(touched_plants);
+      }
     }
 
     //calculate objective
-    double objective = this->calculateObjective();    
+    double objective = this->calculateObjective();
     this->funds = objective;
-    
+
     //add new plants from last round to number_plants
-    this->number_plants_in_service += number_new_plants;
+    this->number_plants_in_service += get_number_new_plants();
     this->number_new_plants = 0;
+
 }
 
 std::pair<Plant*, bool> Game::findBestPlant(Coord person_loc) {
+  if (get_number_plants_in_service() == 0) {
+    return std::pair<Plant*, bool> (NULL, false);
+  }
+  else {
     std::pair<Plant*, double> best_plant(this->plants_in_service.front(), std::numeric_limits<double>::max());
     bool found_plant = false;
     for (Plant* plant : this->plants_in_service) {
-        if (plant->isServiceableCoord(person_loc)) {
-            if (best_plant.second > plant->distanceToCoord(person_loc)) {
-                if (plant->remainingCapacity() > 0) {
-                    //this plant has room to take new person
-                    best_plant.second = plant->distanceToCoord(person_loc);
-                    best_plant.first = plant;
-                    found_plant = true;
-                }
-            }
+      if (plant->isServiceableCoord(person_loc)) {
+        if (best_plant.second > plant->distanceToCoord(person_loc)) {
+          if (plant->remainingCapacity() > 0) {
+            //this plant has room to take new person
+            best_plant.second = plant->distanceToCoord(person_loc);
+            best_plant.first = plant;
+            found_plant = true;
+          }
         }
+      }
     }
     return std::pair<Plant*, bool>(best_plant.first, found_plant);
+  }
+
 }
 
 void Game::processUnservicedElement(int i, int j) {
+  int number_to_service = pop_matrix.numberUnservicedAtCoord(Coord(i, j));
+  if (number_to_service > 0) {
     int number_to_service = pop_matrix.numberUnservicedAtCoord(Coord(i, j));
-    if (number_to_service > 0) {
-        int number_to_service = pop_matrix.numberUnservicedAtCoord(Coord(i, j));
-        std::pair<Plant*, bool> result = findBestPlant(Coord(i,j));
-        while (number_to_service > 0 or !(result.second)) {
-            Plant* new_plant = result.first;
-            int add_to_service = std::min(new_plant->remainingCapacity(), number_to_service);
-            this->addServicedPop(add_to_service);
-            this->pop_matrix.assignUnservicedPop(new_plant, Coord(i,j), add_to_service);
-            new_plant->changeServicedPop(Coord(i,j), add_to_service);
-            number_to_service -= add_to_service;
-            result = findBestPlant(Coord(i,j));
-        }
+    std::pair<Plant*, bool> result = findBestPlant(Coord(i,j));
+    while (number_to_service > 0 and (result.second)) {
+      Plant* new_plant = result.first;
+      int add_to_service = std::min(new_plant->remainingCapacity(), number_to_service);
+      this->addServicedPop(add_to_service);
+      this->pop_matrix.assignUnservicedPop(new_plant, Coord(i,j), add_to_service);
+      new_plant->changeServicedPop(Coord(i,j), add_to_service);
+      number_to_service -= add_to_service;
+      result = findBestPlant(Coord(i,j));
     }
+  }
 }
 
 void Game::processUnservicedPopulation() {
-    for (int i = 0; i < this->pop_matrix.sizeX(); i++) {
-        for (int j = 0; j < this->pop_matrix.sizeY(); j++) {
-        this->processUnservicedElement(i,j);
-        }
+  for (int i = 0; i < this->pop_matrix.sizeX(); i++) {
+    for (int j = 0; j < this->pop_matrix.sizeY(); j++) {
+    this->processUnservicedElement(i,j);
     }
+  }
 }
 
 void Game::addServicedPop(int new_serviced_pop) {
@@ -115,65 +147,65 @@ void Game::addServicedPop(int new_serviced_pop) {
 }
 
 Plant* Game::createPlant(Coord plant_loc) {
-    //update number_new_plants
-    this->number_new_plants++;
-    Plant* new_plant = new Plant(
-        plant_default_capacity, 
-        plant_servable_distance, 
-        plant_loc.x, 
-        plant_loc.y, 
-        this->terrain
-    );
-    this->plants_in_service.push_back(new_plant);
-    return new_plant;
+  this->number_new_plants++;
+  Plant* new_plant = new Plant(
+          plant_default_capacity,
+          plant_servable_distance,
+          plant_loc.x,
+          plant_loc.y,
+          this->terrain
+  );
+  this->plants_in_service.push_back(new_plant);
+  return new_plant;
+
 };
 
 std::queue<Plant*> Game::processServicedPop(
-    Plant* plant, 
-    Coord coord, 
-    std::unordered_map<Plant*, int> serviced_map, 
-    std::queue<Plant*>& queue) 
+  Plant* plant,
+  Coord coord,
+  std::unordered_map<Plant*, int> serviced_map,
+  std::queue<Plant*>& queue)
 {
-    for (std::pair<Plant*, int> mapping : serviced_map) {
-        if (plant->remainingCapacity() > 0) {
-            Plant* old_plant = mapping.first;
-            if (plant->distanceToCoord(coord) < old_plant->distanceToCoord(coord)) {
-                int add_to_service = std::min(plant->remainingCapacity(), mapping.second);
-                this->pop_matrix.moveServicedPopBetweenPlants(old_plant, plant, coord, add_to_service);
-                plant->changeServicedPop(coord, add_to_service);
-                old_plant->changeServicedPop(coord, -add_to_service);                
-                queue.push(old_plant);
-            }
-        }
+  for (std::pair<Plant*, int> mapping : serviced_map) {
+    if (plant->remainingCapacity() > 0) {
+      Plant* old_plant = mapping.first;
+      if (plant->distanceToCoord(coord) < old_plant->distanceToCoord(coord)) {
+        int add_to_service = std::min(plant->remainingCapacity(), mapping.second);
+        this->pop_matrix.moveServicedPopBetweenPlants(old_plant, plant, coord, add_to_service);
+        plant->changeServicedPop(coord, add_to_service);
+        old_plant->changeServicedPop(coord, -add_to_service);
+        queue.push(old_plant);
+      }
     }
-    return queue;
+  }
+  return queue;
 }
 
 std::queue<Plant*> Game::considerNewPlant(Plant* plant, bool touched) {
-    std::queue<Plant*> touched_plants;
-    std::unordered_map<Coord, std::pair<int, std::unordered_map<Plant*, int>>> pop_to_consider
-            = this->pop_matrix.potentialPopForPlant(plant);
-    for (std::pair<Coord, std::pair<int, std::unordered_map<Plant*,int>>> element : pop_to_consider) {
-        Coord coord = element.first;
-        int unserviced = element.second.first;
-        std::unordered_map<Plant*, int> serviced_map = element.second.second;
-        processUnservicedElement(coord.x, coord.y);
-        processServicedPop(plant, coord, serviced_map, touched_plants);
-    }
-    if (!touched) {  //return queue of "touched" plants
-        return touched_plants;
-    } else { //processing a "touched" plant
-        std::queue<Plant*> empty;
-        return empty;
-    }
+  std::queue<Plant*> touched_plants;
+  std::unordered_map<Coord, std::pair<int, std::unordered_map<Plant*, int>>> pop_to_consider
+          = this->pop_matrix.potentialPopForPlant(plant);
+  for (std::pair<Coord, std::pair<int, std::unordered_map<Plant*,int>>> element : pop_to_consider) {
+    Coord coord = element.first;
+    int unserviced = element.second.first;
+    std::unordered_map<Plant*, int> serviced_map = element.second.second;
+    processUnservicedElement(coord.x, coord.y);
+    processServicedPop(plant, coord, serviced_map, touched_plants);
+  }
+  if (!touched) {  //return queue of "touched" plants
+    return touched_plants;
+  } else { //processing a "touched" plant
+    std::queue<Plant*> empty;
+    return empty;
+  }
 };
 
 void Game::processTouchedPlants(std::queue<Plant*> touched_plants) {
-    while (touched_plants.size() > 0) {//process each plant
-        Plant* plant = touched_plants.front();
-        std::queue<Plant*> empty = this->considerNewPlant(plant, true);
-        touched_plants.pop();
-    }
+  while (touched_plants.size() > 0) {//process each plant
+    Plant* plant = touched_plants.front();
+    std::queue<Plant*> empty = this->considerNewPlant(plant, true);
+    touched_plants.pop();
+  }
 }
 
 double Game::calculateObjective() {
