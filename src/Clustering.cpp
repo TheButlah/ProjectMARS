31135ae8f8376193013ab9e915401231f4de24fc
Amit Mizrahi <amit@amizrahi.com>
2018-05-08 15:18:47 -0400

    Distance calculations

--- src/Clustering.cpp
+++ src/Clustering.cpp
@@ -1,4 +1,4 @@
-#include "Game.h"
+#include "../include/Clustering.h"
 
 #include <algorithm>
 #include <cstdlib>
@@ -32,17 +32,50 @@ void Clustering::run(PopulationMatrix popMatrix) {
 			}
 			this->centroids.push(randomCentroid);
 		}
+
+		// Create vectors for each cluster and push them to this->clusters
+
+		for(int i = 0; i < k; i++) {
+			this->clusters.push(std::vector<Coord>());
+		}
 	}	
 	
-	// For each coord in the unserviced matrix, add it to the cluster
-	// corresponding to its closest centroid
+	// Create one data point for each person (unit of population density) in the matrix
 
-	this->clusters = std::vector<std::vector<std::pair>>();
+	std::vector<Coord> dataPoints = std::vector<Coord>();
+
 	for(int i = 0; i < dx; i++) {
 		for(int j = 0; j < dy; j++) {
-			
+			int peopleHere = popMatrix.unserviced_pop_matrix.at(i, j);
+			for(int k = 0; k < peopleHere; k++) {
+				dataPoints.push(Coord(i, j));
+			}
 		}
 	}
+
+	// For each coord in the dataset, add it to the cluster
+	// corresponding to its closest centroid (k-means)
+
+	for(int i = 0; i < dataPoints.size(); i++) {
+
+		Coord point = dataPoints.at(i);
+
+		Coord nearestCentroidIndex;
+		int nearestCentroidDistance = -1;
+
+		for(int c = 0; c < this->centroids->size(); c++) {
+			Coord centroid = this->centroids[c];
+			int dist = (dataPoint.x - centroid.x)*(dataPoint.x - centroid.x) + (dataPoint.y - centroid.y)*(dataPoint.y - centroid.y)
+			if(nearestCentroidDistance == -1 || dist < nearestCentroidDistance) {
+				nearestCentroidDistance = dist;
+				nearestCentroidIndex = c;
+			}
+		}
+
+		this->clusters[nearestCentroidIndex].push(point);
+	}
+
+	// Compute new centroids
 
 }
 
