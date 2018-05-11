13954b6f557db33b4ce28a947fc71c4cde2b6221
Amit Mizrahi <amit@amizrahi.com>
2018-05-11 09:31:47 -0400

    Use max size cluster

--- src/Clustering.cpp
+++ src/Clustering.cpp
@@ -34,23 +34,14 @@ std::pair<std::vector<Coord>, std::vector<std::vector<Coord>>>
 
       for(int i = 0; i < k; i++) {
         Coord randomCentroid = Coord(rand() % dx, rand() % dy);
-
         while(std::find(centroids.begin(), centroids.end(), randomCentroid) != centroids.end()) {
           randomCentroid = Coord(rand() % dx, rand() % dy);
         }
         centroids.push_back(randomCentroid);
       }
 
-      // Create vectors for each cluster and push them to clusters
-
-      for(int i = 0; i < k; i++) {
-        clusters.push_back(std::vector<Coord>());
-      }
-
       clusteredBefore = true;
-    } 
-  
-    assert(clusters.size() == centroids.size());
+    }
 
     // Create one data point for each person (unit of population density) in the matrix
 
@@ -65,11 +56,15 @@ std::pair<std::vector<Coord>, std::vector<std::vector<Coord>>>
       }
     }
 
+    clusters = std::vector<std::vector<Coord>>(); // reset clusters to be empty
+    for(int i = 0; i < k; i++) {
+      clusters.push_back(std::vector<Coord>());
+    }
+
     // For each coord in the dataset, add it to the cluster
     // corresponding to its closest centroid (k-means)
 
     for(int i = 0; i < dataPoints.size(); i++) {
-
       Coord dataPoint = dataPoints.at(i);
 
       int nearestCentroidIndex = 0;
@@ -90,7 +85,7 @@ std::pair<std::vector<Coord>, std::vector<std::vector<Coord>>>
 
     // Compute new centroids
 
-    for(int i = 0; i < clusters.size(); i++) {
+    for(int i = 0; i < k; i++) {
       float sumX = 0.0;
       float sumY = 0.0;
       
@@ -120,54 +115,33 @@ std::pair<std::vector<Coord>, std::vector<std::vector<Coord>>>
   return std::pair<std::vector<Coord>, std::vector<std::vector<Coord>>> (centroids, clusters);
 }
 
-int sparsity(std::vector<Coord> cluster, Coord centroid) {
-  // Sum up all the distances from the centroid
-  int total = 0;
-  for(int i = 0; i < cluster.size(); i++) {
-    total += (cluster.at(i).x - centroid.x)*(cluster.at(i).x - centroid.x) + (cluster.at(i).y - centroid.y)*(cluster.at(i).y - centroid.y);
-  }
-  return total;
-}
-
 std::pair<bool, Coord> Clustering::placePlant(PopulationMatrix popMatrix, int k) {
-  // Take the least "sparse" unserviced cluster above a certain size, if it exists,
-  // and place a plant at its center
+  // Take the largest unserviced cluster and place a plant at its center
 
   std::pair<std::vector<Coord>, std::vector<std::vector<Coord>>> clusterResult = Clustering::run(popMatrix, k);
 
   std::vector<Coord> centroids = clusterResult.first;
   std::vector<std::vector<Coord>> clusters = clusterResult.second;
 
-  std::vector<int> clusterSparsity;
+  int maxSizeIndex = 0;
+  int maxSize = PLACE_PLANT_THRESHOLD;
 
   for(int i = 0; i < clusters.size(); i++) {
-    std::vector<Coord> cluster = clusters.at(i);
-    assert(i < centroids.size());
-    Coord centroid = centroids.at(i);
-    if(cluster.size() > PLACE_PLANT_THRESHOLD) {
-      clusterSparsity.push_back(sparsity(cluster, centroid));
+    if(clusters.at(i).size() > maxSize) {
+      maxSize = clusters.at(i).size();
+      maxSizeIndex = i;
     }
   }
 
-  int minSparsityIndex = 0;
-  int minSparsity = -1;
-
-  for(int i = 0; i < clusterSparsity.size(); i++) {
-    if(minSparsity == -1 || clusterSparsity.at(i) < minSparsity) {
-      minSparsity = clusterSparsity.at(i);
-      minSparsityIndex = i;
-    }
-  }
-
-  bool unservicedClusterExists = clusterSparsity.size()>0;
+  bool unservicedClusterExists = maxSize > PLACE_PLANT_THRESHOLD;
   Coord placement;
-  assert(minSparsityIndex < centroids.size());
+  assert(maxSizeIndex < centroids.size());
 
   if(unservicedClusterExists) {
     placement = Coord(0, 0);
   }
   else {
-    placement = centroids.at(minSparsityIndex);
+    placement = centroids.at(maxSizeIndex);
   }
 
   return std::pair<bool,Coord>(unservicedClusterExists, placement);
