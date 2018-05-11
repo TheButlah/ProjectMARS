d5b5db4ad9af4c6371f91ab8abed723a7d720f5f
Ryan Butler <thebutlah@gmail.com>
2018-05-11 01:15:15 -0400

    Added named arguments for game and coord

--- main/bindings.cpp
+++ main/bindings.cpp
@@ -5,21 +5,41 @@
 namespace py = pybind11;
 
 PYBIND11_PLUGIN(project_mars) {
-	py::module m("project_mars");
+  py::module m("project_mars");
 
-	py::class_<MARS::Coord>(m, "Coord")
-		.def(py::init<int,int>());
+  py::class_<MARS::Coord>(m, "Coord")
+		.def(py::init<int,int>(),
+      "Initializer for Coord",
+      py::arg("x"),
+      py::arg("y"));
+
 
 	py::class_<MARS::Game>(m, "Game")
-		.def(py::init<int,
-	      int,
-	      int,
-	      int,
-	      double,
-	      double,
-	      double,
-	      double,
-	      double>())
-		.def("step", &MARS::Game::step);
+		.def(py::init<
+        int,
+        int,
+        int,
+        int,
+        double,
+        double,
+        double,
+        double,
+        double>(),
+      "Initializer for Game",
+      py::arg("dx"),
+      py::arg("dy"),
+      py::arg("number_of_turns"),
+      py::arg("default_capacity"),
+      py::arg("servable_distance"),
+      py::arg("initial_cost"),
+      py::arg("operating_cost"),
+      py::arg("profit_margin"),
+      py::arg("unserviced_penalty"))
+		.def("step", &MARS::Game::step,
+		  "Advance the game's progress by one time step",
+		  py::arg("add_plant"),
+		  py::arg("plant_coord"));
+
+
 	return m.ptr();
 }
--- python/main.py
+++ python/main.py
@@ -1,10 +1,24 @@
 import numpy as np
 import tensorflow as tf
+import project_mars as pm
 
 
 def main():
-    pass
+  # Game initialization
+
+  game = pm.Game(
+    dx=16,
+    dy=16,
+    number_of_turns=1000,
+    default_capacity=25,
+    servable_distance=3.0,
+    initial_cost=50.0,
+    operating_cost=25.0,
+    profit_margin=5.0,
+    unserviced_penalty=1.0
+  )
+  print(game)
 
 
 if __name__ == "__main__":
-    main()
+  main()
