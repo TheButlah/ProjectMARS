244e61932209bb421887b9ff6827fa17ced0cc1a
shivr2035 <shivr2035@gmail.com>
2018-05-11 21:15:10 -0400

    Push what exists now of the new model

--- src/GrowthPrediction.cpp
+++ src/GrowthPrediction.cpp
@@ -16,12 +16,24 @@ void GrowthPrediction::updateGradients() {
 
   if (t == time_interval) {
     last_pop_matrix = pop_density_matrix;
+    std::unordered_set<Coord> first_pop;
     for (int i = 0; i < last_pop_matrix.numberRows(); i++) {
       for (int j = 0; j < last_pop_matrix.numberCols(); j++) {
-        
+        if (last_pop_matrix.at(i, j) > 0) {
+          first_pop.push_back(Coord(i, j));
+        }
+      }
+    }
+    last_locs_added = first_pop;
+    return;
+  }
+
+  std::unordered_set<Coord> new_pop;
+  for (int i = 0; i < pop_density_matrix.numberRows(); i++) {
+    for (int j = 0; j < pop_density_matrix.numberCols(); j++) {
+      for (pop_density_matrix.at(i, j) > 0 && last_pop_matrix.at(i, j) == 0) {
+        new_pop.push_back(Coord(i, j));
       }
     }
   }
-
-
 }
